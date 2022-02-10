5c63fe5a766e87206f40a10467fa62711d9a5ac6
skqkdld <90442799+skqkdld@users.noreply.github.com>
2022-02-11 01:11:06 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -1,16 +1,7 @@
-$(function () {
+$(function(){
 
-    // for (let i = 0; i < 3; i++) {
-    //     $('.click_btn div').eq(i).click(function () {
-    //         $('.click_btn div').eq(i).css('background-color', '#23a3a7');
-    //         $('.click_btn div').eq(i).css('color', '#fff');
-    //     });
-    // };
-
-    $("#file").on('change',function(){
-        var fileName = $("#file").val();
-        $(".upload-name").val(fileName);
+    $('.bi-text-right').click(function () {
+        $('.toggle_menu').slideToggle();
     });
-
 
 });
